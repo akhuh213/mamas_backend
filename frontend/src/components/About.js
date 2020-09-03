2a4b937c26f825d3fc29b3ca06373caf363e46ab
Alice Huh <aklee213@gmail.com>
2020-09-03 18:39:04 -0400

    [components] Add Navbar component and test

---
+++ frontend/src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return(
+    <div>
+        <h1> About </h1>
+        <p> This is all about us.</p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+    </div>
+    )
+}
+
+
+
+export default About;
