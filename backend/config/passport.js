890d138d77758a3b46cffa788fbf5270abe61701
Alice Huh <aklee213@gmail.com>
2020-09-03 16:58:32 -0400

    [users] Add /login route and test

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -5,17 +5,24 @@ require('dotenv').config();
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
-const { deserializeUser } = require('passport');
+const passport = require('passport')
 const User = mongoose.model('User');
-
-
+// const db = require('../models')
+// options is an object literl containing options to control
+// how the toekn is extracted from the request or verified
 const options = {}
+// jwtFromRequest (REQUIRED) functin that accepts a request as the
+// only parameter and returns either the JWT as a string or null.
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey = process.env.JWT_SECRET;
 
+
+// fromAuthHeaderAsBearerToken(); creates an extractor that looks for the JWT in the auth header
 module.exports = (passport) => {
     passport.use(new JwtStrategy(options, (jwt_payload, done) => {
         User.findById(jwt_payload.id)
+        // jwt_payload is an object literal containing the decoded JWT payload
+        // done is a passport callback that has error first as an argument done(error, user, info)
         .then(user => {
             if (user) {
                 // If the user is found, return null (for error) and user
@@ -27,3 +34,5 @@ module.exports = (passport) => {
         .catch(error => console.log(error));
     }))
 }
+
+
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const port = process.env.PORT || 8000;
 const passport = require('passport');
 
+const users = require('./routes/api/users')
+
 // Middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: false }))
@@ -15,10 +17,13 @@ app.use(passport.initialize());
 // Importing passport file into server
 require('./config/passport')(passport);
 
+
+
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Smile, you are being watch by the Backend Team ' })
 });
 
+app.use('/api/users', users)
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
