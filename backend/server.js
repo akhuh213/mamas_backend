37b6ba9060360c12f7d172e1271b9156c893dc77
Alice Huh <alicehuh@Alices-MacBook-Pro.local>
2020-09-03 13:01:20 -0400

    [passport] Set env variable, set up options, and export passport function

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,15 @@ const cors = require('cors');
 const port = process.env.PORT || 8000;
 const passport = require('passport');
 
-
+// Middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// passport middleware
+app.use(passport.initialize());
+// Importing passport file into server
+require('./config/passport')(passport);
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Smile, you are being watch by the Backend Team ' })
